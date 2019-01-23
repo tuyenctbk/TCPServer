c3f2bba01d7dfcc7070f857fb02df65604ec8c11
tuyenpd4 <tuyenpd4@viettel.com.vn>
2019-01-23 17:05:07 +0700

    190124

---
+++ app/src/main/java/com/seanfarm/tcpserver/Common.kt
@@ -0,0 +1,7 @@
+package com.seanfarm.tcpserver
+
+object Common {
+    val PORT: String = "port"
+    val DEFAULT_PORT: Int = 1010
+
+}
--- app/src/main/java/com/seanfarm/tcpserver/ServerConfigActivity.kt
+++ app/src/main/java/com/seanfarm/tcpserver/ServerConfigActivity.kt
@@ -1,14 +1,19 @@
 package com.seanfarm.tcpserver
 
+import kotlinx.android.synthetic.main.activity_server_config.*
+
 class ServerConfigActivity : BaseActivity() {
 
-    var port: String = ""
+    var port = 0
 
     override fun loadPreferences() {
-        port = pref.
+        port = pref.getInt(Common.PORT, Common.DEFAULT_PORT)
     }
+
     override fun initUI() {
         setContentView(R.layout.activity_server_config)
+
+        etPort.setText(port.toString())
 
     }
 }
